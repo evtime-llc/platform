a2d124f0dfc16df0ee164fb0afa19719a004f243
vahe hatemi <vahehr@gmail.com>
2025-09-30 10:50:29 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+window.SUPABASE_URL = "https://xxxxx.supabase.co";   // replace xxxxx with your project’s actual Supabase URL
+window.SUPABASE_ANON_KEY = "eyJhbGciOi...";          // paste your actual anon key here
